8a85d644dd6573b0b9f7816dd023311f13f7fda6
Ariel Solano <arielsolano@ariels-mbp.lan>
2018-04-25 23:11:16 -0700

    Friends and Survey page Changed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,61 +1,55 @@
 // Data structure that will store the friends data
 var friendsArray = [
   {
-    name: "Scooter",
+    name: "Chandler",
     photo:
-      "http://cdn3-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-15.jpg",
+      "http://c-5uwzmx78pmca09x24kquox2ebdoklvx2evmb.g00.tvguide.com/g00/3_c-5eee.bdocqlm.kwu_/c-5UWZMXPMCA09x24pbbx78x3ax2fx2fkquo.bdoklv.vmbx2fqx2fzx2f0895x2f82x2f80x2f012jln4i-m65i-29i6-72jn-85620899559lx2fzmaqhmx2f528f384x2f490992i0i512n58nink67902i767nki8x2fkpivltmz-nzqmvla-px78-to.rx78ox3fq98k.uizsx3dquiom_$/$/$/$/$/$/$/$/$/$/$",
     scores: ["1", "5", "5", "1", "5", "5", "1", "1", "1", "5"],
   },
   {
-    name: "Fluffy",
-    photo: "https://i.ytimg.com/vi/mRf3-JkwqfU/hqdefault.jpg",
+    name: "Ross",
+    photo: "http://nowhitenoise.com/wp-content/uploads/2013/07/ross-geller.jpg",
     scores: ["2", "4", "1", "1", "1", "1", "1", "2", "1", "5"],
   },
   {
-    name: "Spencer",
-    photo:
-      "http://cdn3-www.cattime.com/assets/uploads/2011/08/best-kitten-names-1.jpg",
+    name: "Joey",
+    photo: "https://media.giphy.com/media/3jArsPD9RBcDm/giphy.gif",
     scores: ["4", "3", "5", "1", "1", "5", "5", "1", "1", "1"],
   },
   {
-    name: "Zackariah",
+    name: "Pheobe",
     photo:
-      "https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg",
+      "https://img.buzzfeed.com/buzzfeed-static/static/2015-06/7/14/campaign_images/webdr13/how-well-do-you-remember-pheobe-buffays-songs-a4cp-2-14119-1433700207-5_dblbig.jpg",
     scores: ["5", "1", "2", "1", "3", "4", "1", "3", "3", "4"],
   },
   {
-    name: "Cloe",
+    name: "Rachel",
     photo:
-      "http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg",
+      "https://res.cloudinary.com/firstnotify/image/upload/c_fill,d_Logo_ysxlov.jpg,f_auto,h_450,q_auto,w_300/jiquwkoqn9iggxzamo6p",
     scores: ["1", "1", "5", "5", "5", "5", "5", "5", "5", "1"],
   },
   {
-    name: "Mrs. Piggy",
+    name: "Ellen",
     photo:
-      "http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg",
+      "https://www.gannett-cdn.com/-mm-/62fff69d8499ae494bfebfbff54095458f42b09c/c=1112-238-2170-836&r=x329&c=580x326/local/-/media/2018/02/13/USATODAY/USATODAY/636541147391206645-Rozman-20180212-15101-1164r.jpg",
     scores: ["1", "1", "5", "1", "3", "2", "5", "1", "1", "5"],
   },
   {
-    name: "Beyonce Knowles",
+    name: "QUEEN BEE",
     photo:
       "http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg",
     scores: ["5", "4", "4", "2", "4", "4", "3", "2", "3", "3"],
   },
   {
-    name: "Adele Adkins",
-    photo: "https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg",
-    scores: ["5", "2", "3", "1", "5", "1", "3", "1", "1", "5"],
-  },
-  {
-    name: "Kermit the Frog",
+    name: "The GOAT - Steph Curry",
     photo:
-      "http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg",
+      "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3975.png&w=350&h=254",
     scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"],
   },
   {
-    name: "Elmo",
+    name: "Oprah",
     photo:
-      "http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png",
+      "https://ia.media-imdb.com/images/M/MV5BNzY4OTEwNjQ3MV5BMl5BanBnXkFtZTgwOTQ3MjQ0NDM@._V1_.jpg",
     scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
   },
 ];
